a817e541fe91299f78dcbfd1f94c0a0ba90f017b
Quentin <quentin.dupuis@mines-ales.org>
2022-01-11 14:38:58 +0100

    Solution implemented to detect more grain which are closed. Add an upper bound to the area condition

--- preprocessing.py
+++ preprocessing.py
@@ -65,37 +65,54 @@ def preprocessing(folder_path, s_img, crop_idx_dim1=1000, thresh_refl=0.15, thre
     # show_image(binary_image)
 
     # Better result without morph_close
-    # binary_image = cv.morphologyEx(binary_image, cv.MORPH_CLOSE, np.ones((2, 2), np.uint8))
-
-    # thresh_image = binary_image.astype(np.uint8)
-    # contours, hierarchy = cv.findContours(thresh_image, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    # plt.imshow(cv.drawContours(im1, contours, -1, (255, 0, 0), 3))
-    # plt.show()
+    # binary_image = cv.morphologyEx(binary_image, cv.MORPH_CLOSE, np.ones((10, 10), np.uint8))
 
     labeled_array = label(binary_image)
     # show_image(labeled_array)
     regions = regionprops(labeled_array)
 
-    list_bbox = [x.bbox for x in regions if x.area >= area_range and x.solidity > 0.9]
+    list_bbox = [x.bbox for x in regions if 12000 >= x.area >= area_range and x.solidity > 0.9]
 
-    # Case if solidity condition is not met
-    list_bbox_bar = [x.bbox for x in regions if x.solidity < 0.9 and x.area >= area_range]
-    ind = 0
-    print(list_bbox_bar[ind])
-    im_bar = imr[list_bbox_bar[ind][0]:list_bbox_bar[ind][2], list_bbox_bar[ind][1]+colmin:list_bbox_bar[ind][3]+colmin]
-    ret_bar, binary_image_bar = cv.threshold(im_bar, thresh_refl, 1, cv.THRESH_BINARY)
-    binary_image_bar = cv.erode(binary_image_bar, np.ones((20, 20), np.uint8))
-    labeled_array_bar = label(binary_image_bar)
-    plt.imshow(labeled_array_bar)
-    plt.show()
-    exit()
+    # Check if two grains have been considered as one (the area is considered for comparison)
+    # list_area = []
+    # list_size = []
+    # for x in regions:
+    #     if x.area >= area_range and x.solidity > 0.9:
+    #         list_area.append(x.area)
+    #         if x.area > 11000:
+    #             list_size.append(x.bbox)
+    # print(sorted(list_area, reverse=True))
 
-    list_bbox = list_bbox + list_bbox_bar
-    list_bbox = np.array(list_bbox)
+    # Case if solidity and size (upper bound) conditions are not met
+    list_bbox_bar = [x.bbox for x in regions if (x.solidity < 0.9 and x.area >= area_range)
+                     or (x.solidity > 0.9 and x.area > 12000)]
+    list_bar = []
+    for ind in range(len(list_bbox_bar)):
+        im_bar = imr[list_bbox_bar[ind][0]:list_bbox_bar[ind][2],
+                     list_bbox_bar[ind][1] + colmin:list_bbox_bar[ind][3] + colmin]  # Select the image inside the bbox
+        ret_bar, binary_image_bar = cv.threshold(im_bar, thresh_refl + 0.1, 1, cv.THRESH_BINARY)  # Higher threshold
+        labeled_array_bar = label(binary_image_bar)
+        regions_bar = regionprops(labeled_array_bar)
+        list_bbox_bar_res = np.array([x.bbox for x in regions_bar if x.area >= area_range + 3500 and x.solidity > 0.9])
+        for i in range(len(list_bbox_bar_res)):
+            list_bbox_bar_res[i][0] = list_bbox_bar_res[i][0] + list_bbox_bar[ind][0]
+            list_bbox_bar_res[i][1] = list_bbox_bar_res[i][1] + list_bbox_bar[ind][1]
+            list_bbox_bar_res[i][2] = list_bbox_bar_res[i][2] + list_bbox_bar[ind][0]
+            list_bbox_bar_res[i][3] = list_bbox_bar_res[i][3] + list_bbox_bar[ind][1]
+        list_bar = [*list_bar, *list_bbox_bar_res]
+    # print(list_bbox_bar_res)
+    # plt.imshow(labeled_array_bar)
+    # plt.show()
+
+    # list_bbox = list_size
+    # list_bbox = list_bar
+    list_bbox = [*list_bbox, *list_bar]
+    array_bbox = np.array(list_bbox)
+
     # Add colmin to y values
-    for i in range(len(list_bbox)):
-        list_bbox[i][1] = list_bbox[i][1] + colmin
-        list_bbox[i][3] = list_bbox[i][3] + colmin
+    for i in range(len(array_bbox)):
+        array_bbox[i][1] = array_bbox[i][1] + colmin
+        array_bbox[i][3] = array_bbox[i][3] + colmin
 
     if verbose:
         fig, ax = plt.subplots()
@@ -103,18 +120,18 @@ def preprocessing(folder_path, s_img, crop_idx_dim1=1000, thresh_refl=0.15, thre
         result = img
         ax.imshow(result)
 
-        for bbox in list_bbox:
+        for bbox in array_bbox:
             x1, y1, x2, y2 = bbox
-            ax.add_patch(patches.Rectangle((y1, x1), y2-y1, x2-x1, fill=False, edgecolor='red', lw=2))
+            ax.add_patch(patches.Rectangle((y1, x1), y2 - y1, x2 - x1, fill=False, edgecolor='red', lw=2))
         plt.imshow(result, cmap="gray")
         plt.show()
 
-    return list_bbox
+    return array_bbox
 
 
 PATH = "D:/Etude technique/"
 sImg = "var8-x75y12_7000_us_2x_2021-10-20T113607_corr"
 
-array_bbox = preprocessing(PATH, sImg)
-# print(array_bbox)
-print(array_bbox.shape)
+array_bbox_ = preprocessing(PATH, sImg)
+# print(array_bbox_)
+print(array_bbox_.shape)
