81284e188160fbbf31a5469aaaccbe27b6ec11f4
skyeran <[kieran.mcnamara@mines-ales.org]>
2022-01-17 12:03:51 +0100

    creating new image with fixe size (padding) for each grain

--- deep_learning.py
+++ deep_learning.py
@@ -1,10 +1,12 @@
 # Global import
+import numpy as np
 import pandas as pd
 import os
 from os import walk
 import spectral as sp
 import torch
-from torch.utils.data import Dataset
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
 
 
 def load(use_path):
@@ -58,6 +60,10 @@ class CustomDataset(Dataset):
         :return: tensor, label
         """
         label = self.labels[idx]
+        if label == '1':
+            label = torch.tensor([1, 0])
+        else:
+            label = torch.tensor([0, 1])
         # path_img = self.paths_img[idx]
         path_hdr = self.paths_hdr[idx]
         img = sp.open_image(path_hdr)
@@ -71,29 +77,29 @@ class CNN(nn.Module):
         super().__init__()
         # 4 conv blocks / flatten / linear / softmax
 
-        # 200, 200, 10 -> 200, 200, 30
-        self.conv1 = nn.Conv2d(in_channels=10, out_channels=30, kernel_size=(5, 5), padding=(2, 2))
-        # 200, 200, 30 -> 100, 100, 30
+        # 180, 180, 10 -> 180, 180, 30
+        self.conv1 = nn.Conv2d(in_channels=10, out_channels=30, kernel_size=(5, 5))
+        # 180, 180, 30 -> 90, 90, 30
         self.pool1 = nn.MaxPool2d(kernel_size=2)
 
-        # 100, 100, 30 -> 100, 100, 60
+        # 90, 90, 30 -> 90, 90, 60
         self.conv2 = nn.Conv2d(in_channels=30, out_channels=60, kernel_size=(5, 5), padding=(2, 2))
-        # 100, 100, 60 -> 50, 50, 60
+        # 90, 90, 60 -> 45, 45, 60
         self.pool2 = nn.MaxPool2d(kernel_size=2)
 
-        # 50, 50, 60 -> 50, 50, 120
+        # 45, 45, 60 -> 45, 45, 120
         self.conv3 = nn.Conv2d(in_channels=60, out_channels=120, kernel_size=(5, 5), padding=(2, 2))
-        # 50, 50,120 -> 25, 25, 120
+        # 45, 45, 120 -> 15, 15, 120
         self.pool3 = nn.MaxPool2d(kernel_size=2)
 
-        # 25, 25, 120 -> 25, 25, 150
+        # 15, 15, 120 -> 15, 15, 150
         self.conv4 = nn.Conv2d(in_channels=120, out_channels=150, kernel_size=(5, 5), padding=(2, 2))
-        # 25, 25, 150 -> 12, 12, 150
+        # 15, 15, 150 -> 5, 5, 150
         self.pool4 = nn.MaxPool2d(kernel_size=2)
 
-        # 12, 12, 150 -> 12, 12, 200
+        # 5, 5, 150 -> 5, 5, 200
         self.conv5 = nn.Conv2d(in_channels=150, out_channels=200, kernel_size=(5, 5), padding=(2, 2))
-        # 12, 12, 200 -> 6, 6, 200
+        # 5, 5, 200 -> 6, 6, 200
         self.pool5 = nn.MaxPool2d(kernel_size=2)
 
         # 6, 6, 200 -> 4, 4, 250
@@ -103,63 +109,60 @@ class CNN(nn.Module):
 
         self.relu = nn.ReLU()
         self.flatten = nn.Flatten()
-        self.linear1 = nn.Linear(1000, 20)
+        self.linear1 = nn.Linear(3*3*250, 20)
         self.linear2 = nn.Linear(20, 20)
         self.linear3 = nn.Linear(20, 2)
         self.softmax = nn.Softmax(dim=1)
 
     def forward(self, input_data):
-        # print("x.shape 1: ", input_data.shape)
+        # print("x.shape 0: ", input_data.shape)
         x = self.conv1(input_data)
-        x = self.tanh(x)
-        # print("x.shape 2: ", x.shape)
+        # print("x.shape 1: ", x.shape)
         x = self.pool1(x)
+        # print("x.shape 1: ", x.shape)
 
-        # print("x.shape: ", x.shape)
         x = self.conv2(x)
-        x = self.tanh(x)
-        # print("x.shape: ", x.shape)
+        # print("x.shape 2: ", x.shape)
         x = self.pool2(x)
+        # print("x.shape 2: ", x.shape)
 
-        # print("x.shape: ", x.shape)
         x = self.conv3(x)
-        x = self.tanh(x)
-        # print("x.shape: ", x.shape)
+        # print("x.shape 3: ", x.shape)
         x = self.pool3(x)
+        # print("x.shape 3: ", x.shape)
 
-        # print("x.shape: ", x.shape)
         x = self.conv4(x)
-        x = self.tanh(x)
-        # print("x.shape: ", x.shape)
+        # print("x.shape 4: ", x.shape)
         x = self.pool4(x)
+        # print("x.shape 4: ", x.shape)
 
-        # print("x.shape: ", x.shape)
         x = self.conv5(x)
-        x = self.tanh(x)
-        # print("x.shape: ", x.shape)
+        # print("x.shape 5: ", x.shape)
         x = self.pool5(x)
+        # print("x.shape 5: ", x.shape)
 
-        # print("x.shape: ", x.shape)
         x = self.conv6(x)
-        x = self.tanh(x)
-        # print("x.shape: ", x.shape)
-        x = self.pool6(x)
+        # print("x.shape 6: ", x.shape)
+        # x = self.pool6(x)
+        # print("x.shape 6: ", x.shape)
 
         x = self.flatten(x)
         x = self.linear1(x)
         x = self.relu(x)
         x = self.linear2(x)
+        x = self.relu(x)
+        x = self.linear3(x)
         x = self.softmax(x)
         return x
 
-use_path_train = "D:\\Etude technique/"
-use_path_test = "D:\\Etude technique"
+use_path_train = "E:\\Etude technique\\raw\\train"
+use_path_test = "E:\\Etude technique\\raw\\test"
 
 df_path_train = load(use_path_train)
 df_path_test = load(use_path_test)
 
-train_set = Custom_Dataset(movement_dir_train)
-test_set = Custom_Dataset(movement_dir_train)
+train_set = CustomDataset(df_path_train)
+test_set = CustomDataset(df_path_test)
 
 epochs = 20
 batch_size = 16
@@ -177,7 +180,7 @@ model = model.double()
 optimizer = torch.optim.Adam(model.parameters())
 criterion = nn.BCELoss(torch.tensor([1, 4]))
 
-print(model)
+# print(model)
 
 for epoch in range(epochs):
     model.train()
@@ -187,18 +190,22 @@ for epoch in range(epochs):
     for batch_num, input_data in enumerate(trainloader):
         optimizer.zero_grad()
         x, y = input_data
-        # print(y.shape)
+        # print(x.shape)
+        x = x.view((16, 10, 180, 180))
+        # print(x.shape)
+        # print(y)
+
+        x = x.to(device).double()
+        y = y.to(device).double()
 
         # y = y.reshape((len(x), 2))
         # print(x.shape, y.shape)
 
-        x = x.to(device).double()
-        y = y.to(device).double()
-        # print('y', y)
-
         output = model(x)
         s = nn.Softmax()
         pred = s(output)
+        # print('pred', pred)
+        # print('y', y)
         for k in range(len(pred)):
             if pred[k][0] > 0.5 and y[k][0] == 1:
                 correct += 1
@@ -217,31 +224,6 @@ for epoch in range(epochs):
         if batch_num % 1 == 0:
             print('\tEpoch %d | Batch %d | Loss %6.2f | Accuracy %6.2f' % (
             epoch, batch_num, loss.item(), correct / total))
-    print('Epoch %d | Loss %6.2f | Accuracy %6.2f' % (epoch, sum(losses) / len(losses), correct / total))
-
-    model.eval()
-    losses = []
-    correct = 0
-    total = 0
-    for batch_num, input_data in enumerate(testloader):
-        x, y = input_data
-        x = x.to(device).double()
-        y = y.to(device).double()
-
-        output = model(x)
-        s = nn.Softmax()
-        pred = s(output)
-        for k in range(len(pred)):
-            if pred[k][0] > 0.5 and y[k][0] == 1:
-                correct += 1
-            elif pred[k][0] < 0.5 and y[k][1] == 1:
-                correct += 1
-            total += 1
-
-        loss = criterion(output, y)
-        losses.append(loss.item())
-
-    print('Validation Epoch %d | Val_Loss %6.2f | Val_Accuracy %6.2f' % (
-    epoch, sum(losses) / len(losses), correct / total))
-
+    print('Epoch %d | Loss %6.2f | Accuracy %6.2f' % (
+          epoch, sum(losses) / len(losses), correct / total))
 
