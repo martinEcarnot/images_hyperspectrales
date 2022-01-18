3cae2af4e690a1cd6b229d0daac54d6f45b57553
skyeran <[kieran.mcnamara@mines-ales.org]>
2022-01-18 15:25:06 +0100

    CNN model train & test loops in function, save & load model

--- deep_learning.py
+++ deep_learning.py
@@ -35,7 +35,6 @@ def load(use_path):
         dataframe = pd.concat([dataframe, data], ignore_index=True)
     return dataframe
 
-
 class CustomDataset(Dataset):
     """
     Create dataset for the network
@@ -149,137 +148,123 @@ class CNN(nn.Module):
 
 use_path_train = "E:\\Etude technique\\raw\\train"
 use_path_test = "E:\\Etude technique\\raw\\test"
+use_path_model = "E:\\Etude technique\\model"
 
-df_path_train = load(use_path_train)
-df_path_test = load(use_path_test)
+def train_model(train_path, verbose=False, epochs=20, batch_size=12):
+    df_path_train = load(train_path)
+    train_set = CustomDataset(df_path_train)
+    trainloader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=0)
 
-train_set = CustomDataset(df_path_train)
-test_set = CustomDataset(df_path_test)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-epochs = 17
-batch_size = 12
+    model = CNN().to(device)
+    model = model.double()
+    optimizer = torch.optim.Adam(model.parameters())
+    criterion = nn.BCELoss()
+    if verbose:
+        print(model)
 
-# Create data loaders
-trainloader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=0)
-testloader = DataLoader(test_set, batch_size=batch_size, shuffle=True, num_workers=0)
+    all_losses = []
+    all_accuracy = []
+    for epoch in range(epochs):
+        model.train()
+        losses, corrects, nbs_0, nbs_1 = [], 0, 0, 0
 
-# test_loader  = DataLoader(test_set,  batch_size=batch_size, shuffle=False)
+        for batch_num, input_data in enumerate(trainloader):
+            optimizer.zero_grad()
+            x, y = input_data
+            x = x.permute(0, 3, 1, 2) / 64
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+            x = x.to(device).double()
+            y = y.to(device).double()
 
-model = CNN().to(device)
-model = model.double()
-optimizer = torch.optim.Adam(model.parameters())
-criterion = nn.BCELoss()
-# print(model)
-all_losses = []
-all_accuracy = []
-for epoch in range(epochs):
-    model.train()
-    losses = []
+            output = model(x)
+            correct, total, nb_0, nb_1 = 0, 0, 0, 0
 
-    corrects = 0
-    nbs_0 = 0
-    nbs_1 = 0
+            for k in range(len(output)):
+                if float(output[k][0].item()) > float(output[k][1].item()):
+                    if int(y[k][0].item()) == 1:
+                        correct += 1
+                        corrects += 1
+                    nb_0 += 1
+                    nbs_0 += 1
+                elif float(output[k][1].item()) >= float(output[k][0].item()):
+                    if int(y[k][1].item()) == 1:
+                        correct += 1
+                        corrects += 1
+                    nb_1 += 1
+                    nbs_1 += 1
+                total += 1
 
-    for batch_num, input_data in enumerate(trainloader):
-        optimizer.zero_grad()
+            loss = criterion(output, y)
+            loss.backward()
+            losses.append(loss.item())
+
+            optimizer.step()
+
+            if verbose:
+                if batch_num % 5 == 0:
+                    print('\tEpoch %d | Batch %d | Loss %6.2f | Accuracy %6.2f' % (
+                          epoch, batch_num, loss.item(), correct / total))
+
+        print('Epoch %d | Loss %6.2f | Accuracy %6.2f' % (
+              epoch, sum(losses) / len(losses), corrects / len(train_set)))
+
+        all_losses.append(sum(losses) / len(losses))
+        all_accuracy.append(corrects / len(train_set))
+
+    if verbose:
+        plt.plot(range(len(all_losses)), all_losses)
+        plt.show()
+        plt.plot(range(len(all_accuracy)), all_accuracy)
+        plt.show()
+
+    return model
+
+def test_model(model_, test_path, verbose=False, batch_size=12):
+    df_path_test = load(test_path)
+    test_set = CustomDataset(df_path_test)
+    testloader = DataLoader(test_set, batch_size=batch_size, shuffle=True, num_workers=0)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    model_.eval()
+    corrects, totals = 0, 0
+
+    for batch_num, input_data in enumerate(testloader):
         x, y = input_data
-        # print(x.shape)
         x = x.permute(0, 3, 1, 2) / 64
-        # img = np.array(x[0][5][:][:])
-        # print(max(img[60]), min(img[60]))
-        # exit()
-
-        # plt.imshow(img)
-        # plt.show()
-        # print(x.shape)
-        # print(y)
 
         x = x.to(device).double()
         y = y.to(device).double()
 
-        # y = y.reshape((len(x), 2))
-        # print(x.shape, y.shape)
+        output = model_(x)
 
-        output = model(x)
-        pred = output
-        # print('pred', pred[0], 'label', y[0])
-        # print('y', y)
-        correct = 0
-        total = 0
-        nb_0 = 0
-        nb_1 = 0
+        correct, total = 0, 0
 
         for k in range(len(output)):
-            # print(label)
-            # print(output[k])
             if float(output[k][0].item()) > float(output[k][1].item()):
                 if int(y[k][0].item()) == 1:
                     correct += 1
                     corrects += 1
-                nb_0 += 1
-                nbs_0 += 1
             elif float(output[k][1].item()) >= float(output[k][0].item()):
                 if int(y[k][1].item()) == 1:
                     correct += 1
                     corrects += 1
-                nb_1 += 1
-                nbs_1 += 1
             total += 1
-        # print('output', s(output))
-        # print('y', y)
-        loss = criterion(output, y)
-        # print(loss)
-        loss.backward()
-        losses.append(loss.item())
+            totals += 1
 
-        optimizer.step()
+    print('Test Accuracy %6.2f' % (corrects / totals))
 
-        if batch_num % 20 == 0:
-            print('\tEpoch %d | Batch %d | Loss %6.2f | Accuracy %6.2f' % (
-                  epoch, batch_num, loss.item(), correct / total))
-    print('Epoch %d | Loss %6.2f | Accuracy %6.2f' % (
-          epoch, sum(losses) / len(losses), corrects / len(train_set)))
+def save_model(model_, save_path):
+    torch.save(model_, save_path)
 
-    all_losses.append(sum(losses) / len(losses))
-    all_accuracy.append(corrects / len(train_set))
+def load_model(load_path):
+    model_ = torch.load(load_path)
+    return model_
 
-plt.plot(range(len(all_losses)), all_losses)
-plt.show()
-plt.plot(range(len(all_accuracy)), all_accuracy)
-plt.show()
+model = train_model(use_path_train)
+test_model(model, use_path_test)
 
-model.eval()
-losses = []
-corrects, totals = 0, 0
-
-for batch_num, input_data in enumerate(trainloader):
-    x, y = input_data
-    x = x.permute(0, 3, 1, 2) / 64
-
-    x = x.to(device).double()
-    y = y.to(device).double()
-
-    output = model(x)
-    pred = output
-
-    correct, total = 0, 0
-
-    for k in range(len(output)):
-        if float(output[k][0].item()) > float(output[k][1].item()):
-            if int(y[k][0].item()) == 1:
-                correct += 1
-                corrects += 1
-        elif float(output[k][1].item()) >= float(output[k][0].item()):
-            if int(y[k][1].item()) == 1:
-                correct += 1
-                corrects += 1
-        total += 1
-        totals += 1
-
-    loss = criterion(output, y)
-    losses.append(loss.item())
-
-print('Test Loss %6.2f | Test Accuracy %6.2f' % (sum(losses) / len(losses), corrects / totals))
-
+save_model(model, use_path_model)
+print('Done')
