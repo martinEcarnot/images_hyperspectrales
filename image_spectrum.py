166833e86e275a6778f225a196b5bec5809c8ae4
skyeran <[kieran.mcnamara@mines-ales.org]>
2022-01-20 09:47:21 +0100

    calculating the brightest band

---
+++ image_spectrum.py
@@ -0,0 +1,35 @@
+import spectral as sp
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
+
+def brightest_band(path, filename):
+    """
+    isolates a small part of the spectralon for each band
+    and calculates its mean brightness
+
+    :param path: the path of directory containing the image
+    :param filename: the image file name
+    :return: the index of the brightest band
+    """
+    img = sp.open_image(path + filename + '.hdr')
+
+    rows = [i for i in range(100)]
+    cols = [i for i in range(150, img.shape[1])]
+
+    means = []
+    for k in tqdm(range(216), desc='calculating the brightest band'):
+        bands = [k]
+
+        band = np.array(img.read_subimage(rows, cols, bands))
+        mean = np.mean(band)
+        means.append(mean)
+
+    return means.index(max(means))
+
+PATH = 'E:\\Etude Technique\\raw\\'
+# PATH = 'D:\\Etude Technique\\'
+file = 'var1-x73y14_7000_us_2x_2021-10-23T151946_corr'
+
+print(brightest_band(PATH, file))
+
