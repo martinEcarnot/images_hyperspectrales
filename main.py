03db0663f3607d6d57c56cf1bbdb5a46cc7a7d04
atndel <etidel3105@gmail.com>
2023-02-17 18:58:19 +0100

    adding a cnns file for different types of cnns and debugging the test_model

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-annot_folder = "img/cropped/"
-#read_all_annot_csv(annot_folder)
-
 from classification_face import *
-annotations_folder = "img/cropped/"
-learning_rate = 5e-4
-epochs = 25
-weight_loss = [2., 2.,2.]
-main_loop(annotations_folder, 'Face',weight_loss, learning_rate, epochs=epochs, batch_size=8, other_class = False, bands = [22, 53, 89]
-          )
+from cnns import *
+annot_folder = "img/cropped/RGB"
+learning_rates = [1e-5,5e-4,1e-4,5e-3,1e-3]
+list_n_epochs = [40,50,60,70,80]
+weight_loss = [2., 2.]
+i = 0
+model_fn = "CNN_1_"
+for n_epochs in list_n_epochs:
+    for lr in learning_rates:
+        main_loop(annotations_folder,model_fn+str(i),'Face',weight_loss, learning_rate, epochs=epochs, batch_size=20, other_class = False)
+        i+=1
